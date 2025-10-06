797aafb343fbcf151bc9bf32f80e6da773be9cd1
markchou1205-uch <markchou1205@uch.edu.tw>
2025-10-06 09:56:03 +0800

    Update advanced-calculations.js

--- scripts/advanced-calculations.js
+++ scripts/advanced-calculations.js
@@ -685,3 +685,5 @@ if (typeof module !== 'undefined' && module.exports) {
         AdvancedCalculator
     };
 }
+// 若類別中有 static GUA_64_COMPLETE，這行會讓它成為全域變數
+window.GUA_64_COMPLETE = DivinationTutorial.GUA_64_COMPLETE;
